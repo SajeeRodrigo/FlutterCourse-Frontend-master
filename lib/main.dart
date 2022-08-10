81ccffb04cc9a59987f9327036f776a6636b4be8
SajeeRodrigo <sajee.thamanga@gmail.com>
2022-08-10 16:03:57 +0530

    page rendering

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
-import 'package:coffee_masters/offerspage.dart';
+import 'package:coffee_masters/pages/offerspage.dart';
 import 'package:flutter/material.dart';
+
+import 'pages/menupage.dart';
+import 'pages/orderpage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -26,26 +29,28 @@ class _GreetState extends State<Greet> {
 
   @override
   Widget build(BuildContext context) {
-    var greetStyle = const TextStyle( 
+    var greetStyle = const TextStyle(
       fontSize: 24,
-);
+    );
     return Column(
       children: [
         Padding(
           padding: const EdgeInsets.all(8.0),
           child: Row(
             children: [
-              Text("Hello $name", 
-              style: greetStyle ,
+              Text(
+                "Hello $name",
+                style: greetStyle,
               ),
-              Text("!!!!",
-              style: greetStyle,
+              Text(
+                "!!!!",
+                style: greetStyle,
               )
             ],
           ),
         ),
         Padding(
-          padding: const EdgeInsets.only(right:24.0, left: 24.0),
+          padding: const EdgeInsets.only(right: 24.0, left: 24.0),
           child: TextField(
               onChanged: (value) => setState(() {
                     name = value;
@@ -85,23 +90,52 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
 
-
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  var selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
+    Widget currentWidgetPage = const Text("!!!");
+
+    switch (selectedIndex) {
+      case 0:
+        currentWidgetPage = const MenuPage();
+        break;
+      case 1:
+        currentWidgetPage = const OffersPage();
+        break;
+      case 2:
+        currentWidgetPage = const OrderPage();
+        break;
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Image.asset("images/logo.png"),
       ),
-      bottomNavigationBar: BottomNavigationBar(items: [
-        BottomNavigationBarItem(icon: Icons.)
-      ]),
-      body: const OffersPage(),
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: selectedIndex,
+          onTap: (newIndex) {
+            setState(() {
+              selectedIndex = newIndex; //changing the state
+            });
+          },
+          backgroundColor: Theme.of(context).primaryColor,
+          selectedItemColor: Colors.yellow.shade400,
+          unselectedItemColor: Colors.brown.shade50,
+          items: const [
+            BottomNavigationBarItem(label: "Menu", icon: Icon(Icons.coffee)),
+            BottomNavigationBarItem(
+                label: "Offers", icon: Icon(Icons.local_offer)),
+            BottomNavigationBarItem(
+                label: "Order",
+                icon: Icon(Icons.shopping_cart_checkout_outlined)),
+          ]),
+      body: currentWidgetPage,
+      //  const OffersPage(),
     );
   }
 }
